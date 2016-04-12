77158539e21afb35de34894f8458295bc3ac6188
Rogier Nitschelm <rogiernitschelm@live.nl>
2016-04-12 15:52:20 +0200

    email control

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160412120735) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
