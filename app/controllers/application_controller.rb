8a9dfeb541a3b034d7488b43a0ec15b0977bc325
Rogier Nitschelm <rogiernitschelm@live.nl>
2016-04-12 16:03:01 +0200

    add rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,6 +7,10 @@ class ApplicationController < ActionController::Base
 
  # Make the controller method available in the views
  helper_method :current_user
+
+ def record_not_found
+    redirect_to action: :index
+  end
 
   def current_user
     return unless session[:user_id].present?
